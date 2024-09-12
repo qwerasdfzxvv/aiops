9e34c9461495b304e53ec1abc5b6055ac60d2e49
qwerasdfzxvv <1034009199@qq.com>
2024-09-12 13:37:54 +0800

    Add files via upload

---
+++ view-iportal/pipeline.py
@@ -0,0 +1,405 @@
+data = {
+    "dp_type_a": {
+        "columnName": "力没联还发布",
+        "x": 5,
+        "y": 5,
+        "columnsNodes": [
+            {
+                "queueId": "3Ff9Af6B-b7b5-D644-44A9-5Ad4dFdf159E",
+                "name": "高世领",
+                "status": 2,
+                "label": "内具存",
+                "x": 5,
+                "y": 50
+            },
+            {
+                "queueId": "F0F905bC-748e-9B30-4Ce4-aEedf0BE071a",
+                "name": "准机",
+                "status": 4,
+                "label": "打物铁",
+                "x": 5,
+                "y": 120
+            },
+            {
+                "queueId": "88A3a461-9dB9-4Ad3-E3BC-D93DAd32794e",
+                "name": "因生少",
+                "status": 2,
+                "label": "放手",
+                "x": 5,
+                "y": 190
+            },
+            {
+                "queueId": "1Ed81Ed1-15Ba-fF37-00b5-Bc6eC70CC19b",
+                "name": "石精价",
+                "status": 4,
+                "label": "声此",
+                "x": 5,
+                "y": 260
+            },
+            {
+                "queueId": "B9FC43Ae-4D39-BA8d-ecbE-2FA02fBF5bFc",
+                "name": "统此",
+                "status": 3,
+                "label": "关指动",
+                "x": 5,
+                "y": 330
+            },
+            {
+                "queueId": "f92fe264-c26d-fABA-33C6-4bAF0174B107",
+                "name": "华布但",
+                "status": 5,
+                "label": "问声连",
+                "x": 5,
+                "y": 400
+            },
+            {
+                "queueId": "bb897D63-5677-Ece3-F5F7-1F10D63151BB",
+                "name": "认体山边",
+                "status": 5,
+                "label": "并给",
+                "x": 5,
+                "y": 470
+            },
+            {
+                "queueId": "1461E52A-E6f6-Ea4B-143C-E0Ff2ccef922",
+                "name": "验型",
+                "status": 1,
+                "label": "根往",
+                "x": 5,
+                "y": 540
+            },
+            {
+                "queueId": "9e8Ff3cB-1FEF-9f61-7ff1-fC15dF3faB7b",
+                "name": "系道多",
+                "status": 5,
+                "label": "严再众养",
+                "x": 5,
+                "y": 610
+            },
+            {
+                "queueId": "8Ae4A9cb-FF49-CC84-c7AF-8Bc5E6F9235f",
+                "name": "各整",
+                "status": 3,
+                "label": "质持思",
+                "x": 5,
+                "y": 680
+            },
+            {
+                "queueId": "EDbdFfFE-0b69-b5Cb-1dEe-EdE943b75eE8",
+                "name": "用点",
+                "status": 5,
+                "label": "通它县",
+                "x": 5,
+                "y": 750
+            }
+        ]
+    },
+    "dp_type_b": {
+        "columnName": "力圆发布",
+        "x": 150,
+        "y": 5,
+        "columnsNodes": [
+            {
+                "queueId": "0d01BE01-0D73-c68E-4dfc-a64EBc4C376C",
+                "name": "或经具",
+                "ip": "88.212.84.57",
+                "cluster": "saboi",
+                "status": 1,
+                "label": "省议权",
+                "x": 150,
+                "y": 50
+            },
+            {
+                "queueId": "2857cf1e-E8bC-6c45-146F-E9C7fe7dFfEf",
+                "name": "口时形",
+                "ip": "162.98.139.88",
+                "cluster": "wkxpl",
+                "status": 3,
+                "label": "而济成接",
+                "x": 150,
+                "y": 120
+            },
+            {
+                "queueId": "d22cFABA-D1a4-7FEA-8deD-a6aaeeaBB57E",
+                "name": "代史",
+                "ip": "89.247.101.157",
+                "cluster": "qqcop",
+                "status": 2,
+                "label": "查整少",
+                "x": 150,
+                "y": 190
+            },
+            {
+                "queueId": "7B863b26-e2F8-0105-31DE-BE96EaF9FfBC",
+                "name": "时国消",
+                "ip": "80.251.60.130",
+                "cluster": "xpuzo",
+                "status": 4,
+                "label": "当须局",
+                "x": 150,
+                "y": 260
+            }
+        ]
+    },
+    "dp_type_c": {
+        "columnName": "们万南发布",
+        "x": 300,
+        "y": 5,
+        "columnsNodes": [
+            {
+                "queueId": "65E0F3E5-61D6-4C38-F5C9-0efB2BDFFEef",
+                "name": "长增整求",
+                "namespace": "治叫进",
+                "deployment": "公适这-先放上金-@number(1,5)",
+                "ip": "183.186.167.189",
+                "cluster": "boqlz",
+                "status": 3,
+                "label": "去国群",
+                "x": 300,
+                "y": 50
+            },
+            {
+                "queueId": "abcb215f-D250-93Fe-C29e-7f55bc322Bc9",
+                "name": "连道圆",
+                "namespace": "对几",
+                "deployment": "持期干-整具-@number(1,5)",
+                "ip": "164.57.195.140",
+                "cluster": "ebjds",
+                "status": 2,
+                "label": "按论维行",
+                "x": 300,
+                "y": 120
+            },
+            {
+                "queueId": "F2DC1EdB-79CE-DEA5-68F3-dEf7D433E71C",
+                "name": "头府红",
+                "namespace": "动那易位",
+                "deployment": "业断线-备放温划-@number(1,5)",
+                "ip": "10.228.169.208",
+                "cluster": "bjozn",
+                "status": 3,
+                "label": "矿把造",
+                "x": 300,
+                "y": 190
+            },
+            {
+                "queueId": "89AAEB8a-295b-790A-9Db0-A0c89f7DBeDf",
+                "name": "点设与",
+                "namespace": "世作",
+                "deployment": "自没已-生办马-@number(1,5)",
+                "ip": "141.190.47.71",
+                "cluster": "ejcep",
+                "status": 3,
+                "label": "把回",
+                "x": 300,
+                "y": 260
+            },
+            {
+                "queueId": "93BcdDFF-2861-Ed55-69D0-Cf6cC7EE89CF",
+                "name": "研那月关",
+                "namespace": "心解",
+                "deployment": "气毛-难比-@number(1,5)",
+                "ip": "236.82.22.123",
+                "cluster": "ancfe",
+                "status": 4,
+                "label": "本一片",
+                "x": 300,
+                "y": 330
+            },
+            {
+                "queueId": "cbe9a97d-B04e-1b8E-998d-E65ceac33a2C",
+                "name": "两选电",
+                "namespace": "料划海",
+                "deployment": "离量情合-及质-@number(1,5)",
+                "ip": "145.23.130.165",
+                "cluster": "hqlyz",
+                "status": 2,
+                "label": "活主济",
+                "x": 300,
+                "y": 400
+            }
+        ]
+    },
+    "dp_type_d": {
+        "columnName": "通定克发布",
+        "x": 450,
+        "y": 5,
+        "columnsNodes": [
+            {
+                "queueId": "6EeCA941-19e7-12D4-60Ca-dD25DCBB92Ce",
+                "name": "运便主水",
+                "namespace": "今打",
+                "deployment": "小品-者准接先-@number(1,5)",
+                "ip": "151.12.122.188",
+                "cluster": "zuyhl",
+                "status": 2,
+                "label": "矿听",
+                "x": 450,
+                "y": 50
+            },
+            {
+                "queueId": "e4BeE62F-23Ce-09BF-0fbf-8A0a841dfAA4",
+                "name": "应之",
+                "namespace": "说山图",
+                "deployment": "具求参-体角-@number(1,5)",
+                "ip": "155.246.180.156",
+                "cluster": "tjuel",
+                "status": 4,
+                "label": "小性务",
+                "x": 450,
+                "y": 120
+            }
+        ]
+    },
+    "dp_type_f": {
+        "columnName": "没压世发布",
+        "x": 600,
+        "y": 5,
+        "columnsNodes": [
+            {
+                "queueId": "ff2Fdcd9-Fe0F-DeCA-C3EB-828Abd4C3aF2",
+                "name": "白组史",
+                "namespace": "农省",
+                "deployment": "共产-空况-@number(1,5)",
+                "ip": "34.92.19.35",
+                "cluster": "mhwtj",
+                "status": 3,
+                "label": "或半",
+                "x": 600,
+                "y": 50
+            },
+            {
+                "queueId": "7B41dfd1-FbdF-AfBa-CaCc-98b2Df87bAf7",
+                "name": "具格二",
+                "namespace": "导老",
+                "deployment": "再级置也-认资断-@number(1,5)",
+                "ip": "90.33.5.77",
+                "cluster": "iobor",
+                "status": 5,
+                "label": "前易",
+                "x": 600,
+                "y": 120
+            },
+            {
+                "queueId": "B7ec3D5b-5b7D-BAF6-bf9e-aAf943A079AC",
+                "name": "或列例",
+                "namespace": "酸们引据",
+                "deployment": "高之第及-教无向-@number(1,5)",
+                "ip": "241.156.205.25",
+                "cluster": "ifthx",
+                "status": 2,
+                "label": "示矿",
+                "x": 600,
+                "y": 190
+            }
+        ]
+    }
+}
+
+dp_type_map = {
+    'dp_type_a': '类型a发布',
+    'dp_type_b': '类型b发布',
+    'dp_type_c': '类型c发布',
+    'dp_type_d': '类型d发布',
+    'dp_type_e': '类型e发布',
+    'dp_type_f': '类型f发布',
+    'dp_type_g': '类型g发布',
+    'dp_type_h': '类型h发布',
+}
+
+start_x = 50
+start_y = 50
+padding = 50
+node_width = 150
+node_height = 50
+
+nodes = []
+edges = []
+second_row=[]
+for index,item in enumerate(list(data.keys())):
+    tt = [{"id": item, "name": dp_type_map[item]}] + data[item]['columnsNodes']
+    for index_c, column in enumerate(tt):
+        node={
+            "id": column.get('queueId') or column['id'],
+            "shape": "pipeline-node" if column.get('queueId') else 'rect',
+            "x": start_x + padding + index * (node_width + padding * 2),
+            "y": start_y + node_height + padding / 2 + index_c * (node_height + padding / 2),
+            "width": node_width,
+            "height": node_height,
+            "label": column['name'],
+            "data": column,
+            "attrs": {
+                "body": {
+                    "stroke": "#8f8f8f",
+                    "strokeWidth": 1,
+                    "fill": "red",
+                    # "rx": 30,
+                }
+            }
+        }
+        nodes.append(node)
+        if index_c==1:
+            second_row.append({"x": node['x']-padding, "y": node['y']+node_height/2})
+print(second_row)
+dp_type=list(data.keys())
+for i in range(len(dp_type)+1):
+    edges.append({
+        "source": [start_x+i*(node_width+padding*2), start_y],
+        "target": [start_x+i*(node_width+padding*2), 1000],
+        "attrs": {
+            "line": {
+                "stroke": "#8f8f8f",
+                "strokeWidth": 1,
+                "targetMarker": ''
+            }
+        }
+    })
+
+edges.append({
+        "source": second_row[0],
+        "target": {"x": second_row[-1]['x']+node_width+padding*2, "y": second_row[-1]['y']},
+        "sourceAnchor": 1,
+        "zIndex": -1,
+        "attrs": {
+            "line": {
+                "stroke": "#8f8f8f",
+                "strokeWidth": 1,
+                "targetMarker": ''
+            }
+        }
+    })
+# print(second_row)
+for k, v in data.items():
+    for i, j in list(zip(v['columnsNodes'][:-1], v['columnsNodes'][1:])):
+        for lr in ['left', 'right']:
+            edges.append({
+                "source": {
+                    "cell": i['queueId'],
+                },
+                "target": {
+                    "cell": j['queueId'],
+                },
+                "attrs": {
+                    "line": {
+                        "stroke": "#8f8f8f",
+                        "strokeWidth": 1,
+                        "targetMarker": ''
+                    }
+                },
+                "router": {
+                    "name": "oneSide",
+                    "args": {
+                        "padding": 10,
+                        "side":lr
+                    }
+                }
+            })
+import json
+
+# print(json.dumps(nodes,indent=1,ensure_ascii=False))
+# print(json.dumps(edges,indent=1,ensure_ascii=False))
+
+print('export const data=', json.dumps({
+    "nodes": nodes,
+    "edges": edges
+}, ensure_ascii=False))
